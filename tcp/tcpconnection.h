7bc54d14e69ea5b270d301c561e522dbb3bff0d7
Don Silvio <svobodacom1@gmail.com>
2024-07-09 20:42:21 +0300

    write all methods in tcpconnection class

--- tcp/tcpconnection.h
+++ tcp/tcpconnection.h
@@ -10,10 +10,23 @@ class TcpConnection : public QObject
     Q_OBJECT
 public:
     explicit TcpConnection(QObject *parent = nullptr);
+    ~TcpConnection();
 
     virtual void setSocket(QTcpSocket* socket);
 
+protected:
+    QTcpSocket* m_socket;
+    QTcpSocket *getSocket();
+
 signals:
+
+public slots:
+    virtual void connected();
+    virtual void disconnected();
+    virtual void readyRead();
+    virtual void bytesWritten(qint64 bytes);
+    virtual void stateChanged(QAbstractSocket::SocketState socketState);
+    virtual void error(QAbstractSocket::SocketError socketError);
 };
 
 #endif // TCPCONNECTION_H
